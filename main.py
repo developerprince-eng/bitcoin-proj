19f382efd4919c3861f5155075270e7066585a5e
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-01-20 23:28:12 +0200

    changes

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ def main():
     df1= dataset.__read_csv__(path='graphs/inputs/bitcoin_price.csv')
     df2= dataset.__read_csv__(path='graphs/inputs/ethereum_price.csv')
 
-    df1.plot(kind='line', x='Date', y='High', ax=ax)
-    df2.plot(kind='line', x='Date', y='High', ax=ax)
+    df1 = df1.iloc[::-1]
+    df2 = df2.iloc[::-1]
+    
+    df1.plot(kind='line', x='Date', y='High', ax=ax, legend='BitCoin')
+    df2.plot(kind='line', x='Date', y='High', ax=ax, legend='Ethereum')
 
     plt.show()
 
